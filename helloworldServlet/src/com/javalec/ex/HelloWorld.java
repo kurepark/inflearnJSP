26389b1a79d7d25204241216d7a459f9980e9526
kurepark <eunhye0406@gmail.com>
2018-05-26 22:03:50 +0900

    서블릿 맛보기 예제.

---
+++ helloworldServlet/src/com/javalec/ex/HelloWorld.java
@@ -0,0 +1,44 @@
+package com.javalec.ex;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class HelloWorld
+ */
+@WebServlet("/HWorld")//어노테이션을 이용한 닉네임이 정해짐
+public class HelloWorld extends HttpServlet {//클래스와 httpservlet 상속
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public HelloWorld() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+    
+    // doget, dopost메소드, 서블릿을 실행시키면 두개의 메소드 안에 있는 내용이 실행된다.
+    
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		System.out.println("HelloWorld~~~~~");
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
